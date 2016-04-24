6b3bd46734de43b5ce3a1517194450f602f575ce
Valentine Michael Smith <redditor.vmsmith@gmail.com>
2016-04-24 14:35:07 -0400

    Create EDA.R

---
+++ EDA.R
@@ -0,0 +1,51 @@
+charity <- read.csv("./charity.csv")
+str(charity)
+
+# home
+hist(charity$home)
+
+# chld
+hist(charity$chld)
+
+# hinc
+hist(charity$hinc)
+
+# genf
+hist(charity$genf)
+
+# wrat
+hist(charity$wrat)
+
+# avhv
+hist(charity$avhv)
+
+# incm
+hist(charity$incm)
+
+# inca
+hist(charity$inca)
+
+# plow
+hist(charity$plow)
+
+# npro
+hist(charity$npro)
+
+# tgif
+hist(charity$tgif, breaks = 50)
+
+# lgif
+hist(charity$lgif, breaks = 50)
+
+# rgif
+hist(charity$rgif, breaks = 50)
+
+# tdon
+hist(charity$tdon)
+
+# tlag
+hist(charity$tlag)
+
+# agif
+hist(charity$agif)
+
